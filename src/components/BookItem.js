e1b1cabf4562fc714a925030dcc7b32f1493d014
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-02-23 21:52:58 +0900

    React Props

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,12 +1,17 @@
-
-function BookItem() {
+function BookItem(props) {
   return (
     <div className="content-area">
-      <div className="book-title"><h2>book Title</h2></div>
-      <div className="author">Author</div>
-      <div className="book-description"><p>Description</p></div>
+      <div className="book-quote">
+        <h3>{props.data.quote}</h3>
+      </div>
+      <div className="book-title">
+        <p>{props.data.title}</p>
+      </div>
+      <div className="book-author">
+        <p>{props.data.author}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default BookItem
+export default BookItem;
