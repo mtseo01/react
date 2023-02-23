13de0bfdccb93b864bbeded7e16244a2b3c3da6d
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-02-23 21:18:30 +0900

    React Component

---
+++ src/components/BookItem.js
@@ -0,0 +1,12 @@
+
+function BookItem() {
+  return (
+    <div className="content-area">
+      <div className="book-title"><h2>book Title</h2></div>
+      <div className="author">Author</div>
+      <div className="book-description"><p>Description</p></div>
+    </div>
+  )
+}
+
+export default BookItem
