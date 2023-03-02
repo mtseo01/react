acb0d47035abdd476a20c8983ba28f27ace4b647
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-02 16:20:45 +0900

    Add book Schema (back-end)

---
+++ back/src/api/models/book.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+export interface IBook {
+  _id: string;
+  title: string;
+  author: string;
+  quote: string;
+  imgUrl: string;
+  likes: number;
+}
+
+export interface IBookModel extends IBook {}
+
+const bookSchema = new mongoose.Schema<IBook, IBookModel>({
+  _id: mongoose.Schema.Types.ObjectId,
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  quote: { type: String, required: true },
+  imgUrl: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+});
+
+export default mongoose.model<IBookModel>('Book', bookSchema);
