255211238b6b6ea67242411cf0fcc2f47d68d5d3
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-12 00:43:09 +0900

    Apply Add Button component

--- src/pages/books/BookListPage.tsx
+++ src/pages/books/BookListPage.tsx
@@ -1,20 +1,21 @@
 import BookItem from '../../components/books/BookItem';
+import AddButton from '../../components/UI/AddButton';
 import books from '../../assets/data';
 
-const ListPage = () => {
+function ListPage() {
   const alertTitle = (title: string): void => {
     alert(title);
   };
   return (
-    <div>
-      <h1>list page</h1>
-      <div>
+    <div className="list-page">
+      <div className="content">
         {books.map((book) => (
           <BookItem key={book.id} data={book} alertTitle={alertTitle} />
         ))}
       </div>
+      <AddButton />
     </div>
   );
-};
+}
 
 export default ListPage;
