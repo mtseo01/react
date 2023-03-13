43b64a897496b8d5b8b2fc2e129ec2b39837730d
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-13 13:06:38 +0900

    Feat: Book List Page (Fetch Data)

--- src/pages/books/BookListPage.tsx
+++ src/pages/books/BookListPage.tsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from 'react';
+
+// components
 import BookItem from '../../components/books/BookItem';
 import AddButton from '../../components/UI/AddButton';
-import books from '../../assets/data';
+
+// types
+import { Books } from '../../assets/types';
+
+// APIs
+import { getBooksAll } from '../../api/books';
 
 function ListPage() {
-  const alertTitle = (title: string): void => {
-    alert(title);
-  };
+  const [books, setBooks] = useState<Books[]>([]);
+
+  // 책 리스트 데이터 불러오기
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await getBooksAll();
+        setBooks(res.data.docs);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="list-page">
       <div className="content">
-        {books.map((book) => (
-          <BookItem key={book.id} data={book} alertTitle={alertTitle} />
-        ))}
+        {books && books.map((book) => <BookItem key={book._id} data={book} />)}
       </div>
       <AddButton />
     </div>
