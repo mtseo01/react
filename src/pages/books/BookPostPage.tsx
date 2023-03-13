cc52801798a50c9bd0d4b8931f57e47b04c821ae
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-13 11:51:49 +0900

    Feat: Create Post

--- src/pages/books/BookPostPage.tsx
+++ src/pages/books/BookPostPage.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// component
 import PostForm from '../../components/books/PostForm';
 
+// API
+import { createBook } from '../../api/books';
+
 function BookPostPage() {
+  const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [imgUrl, setImgUrl] = useState('');
   const [quote, setQuote] = useState('');
+
   const titleChangeHandler = (event: any) => {
     setTitle(event.target.value);
   };
@@ -18,28 +26,41 @@ function BookPostPage() {
   const quoteChangeHandler = (event: any) => {
     setQuote(event.target.value);
   };
+
   // 제출
-  const clickPostBtnHandler = () => {
+  const clickPostBtnHandler = async () => {
     if (
       title.trim() === '' ||
       author.trim() === '' ||
       imgUrl.trim() === '' ||
       quote.trim() === ''
     ) {
-      alert('모두 입력해주세요');
+      alert('모두 입력해 주세요.');
       return;
+    } else {
+      const data = {
+        title,
+        author,
+        imgUrl,
+        quote,
+      };
+      try {
+        // Book 생성하기
+        const res = await createBook(data);
+        console.log(res);
+
+        setTitle('');
+        setAuthor('');
+        setImgUrl('');
+        setQuote('');
+
+        // 리스트 페이지로 이동
+        navigate('/books');
+      } catch (error) {
+        // 에러 처리
+        console.log(error);
+      }
     }
-    const data = {
-      title,
-      author,
-      imgUrl,
-      quote,
-    };
-    console.log(data);
-    setTitle('');
-    setAuthor('');
-    setImgUrl('');
-    setQuote('');
   };
 
   return (
