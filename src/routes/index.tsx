046a58dae18f0489d88feca45108a40c675cece9
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-13 14:32:52 +0900

    Add Post Page Router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import RootLayout from '../pages/books/RootLayout';
 import BookListPage from '../pages/books/BookListPage';
 import BookDetailPage from '../pages/books/BookDetailPage';
-
+import BookPostPage from '../pages/books/BookPostPage';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
       { path: '/', element: <Navigate to="/books" /> },
       { path: '/books', element: <BookListPage /> },
       { path: '/books/detail/:id', element: <BookDetailPage /> },
+      { path: '/books/post', element: <BookPostPage /> },
     ],
   },
 ]);
