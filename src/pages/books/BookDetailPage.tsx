bfe96d6e909d24b0ba0a4cff0e501a82e9fbf49a
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-03-16 19:10:37 +0900

    Feat: Delete Book Post

--- src/pages/books/BookDetailPage.tsx
+++ src/pages/books/BookDetailPage.tsx
@@ -1,17 +1,33 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Book from '../../components/books/Book';
 
 // types
 import { Books } from '../../assets/types';
 
 // API
-import { getBook } from '../../api/books';
+import { getBook, deleteBook } from '../../api/books';
 
 function BookDetailPage() {
   const [book, setBook] = useState<Books>();
   let { id } = useParams();
+  const navigate = useNavigate();
 
+  // 포스트 삭제
+  const clickDeleteBtnHandler = async () => {
+    // eslint-disable-next-line no-restricted-globals
+    if (!confirm('정말 삭제하시겠습니까?')) return;
+    try {
+      const res = deleteBook(id);
+      console.log(res);
+      navigate('/books');
+    } catch (error) {
+      // 에러 핸들하기
+      console.log(error);
+    }
+  };
+
+  // 데이터 불러오기
   useEffect(() => {
     async function fetchData() {
       try {
@@ -26,7 +42,13 @@ function BookDetailPage() {
   return (
     <div>
       <h1>detail</h1>
-      {book && <Book data={book} />}
+      <div>{book && <Book data={book} />}</div>
+      <div>
+        <button onClick={() => navigate('/books/update/' + id)}>
+          수정하기
+        </button>
+        <button onClick={clickDeleteBtnHandler}>삭제하기</button>
+      </div>
     </div>
   );
 }
